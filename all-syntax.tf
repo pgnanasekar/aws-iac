96d06d3d28086124e0ea21f4b8014c8dd8a0f833
pgnanasekar <pgsekar@gmail.com>
2023-04-02 23:09:20 +0530

    Adding syntax

--- all-syntax.tf
+++ all-syntax.tf
@@ -1,4 +1,4 @@
-#AWS official provider configuration
+#provider configuration
 terraform {
  required_providers {
    aws = {                      # provider local name
@@ -8,8 +8,8 @@ terraform {
  }
 }
 
-# Configure the AWS Provider don't forget to replace the keys
- provider "aws" {
+# Configure the AWS Provider
+provider "aws" {
  region = "us-central-1" # provider configuration options
 }
 
@@ -37,8 +37,37 @@ resource "aws_instance" "MyFirstInstance" {
   }
 }
 
+resource "aws_eip" "lb" {
+  vpc = true
+}
+
+output "eip" {
+  value = aws_eip.lb.public_ip
+}
+
+#allocate the EIP to instance 
+resource "aws_eip_association" "eip_assoc" {
+  instance_id = aws_instance.MyFirstInstance.id
+  allocation_id = aws_eip.lb.id
+}
+
+#Creating new security group and allow the particular EIP in inbound rules
+resource "aws_security_group" "allow_tls" {
+  name = "test-sg"
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["${aws_eip.lb.public_ip}/32"]
+  }
+}
+
 #S3 Bucket Creation 
 resource "aws_s3_bucket" "data" {
   bucket = "webserver-bucket"
   acl = "private"
 } 
+
+output "mys3bucket" {
+  value = aws_s3_bucket.data.bucket_domain_name
+}
